664d9e83f36c3c91f8e615e04d4ced4fae187a18
Divij-BTS <dsanjanwala@biotechsquare.com>
2022-05-16 20:17:18 +0530

    no job posting

--- src/components/jobposting/JobPosting.js
+++ src/components/jobposting/JobPosting.js
@@ -12,24 +12,29 @@ export default function JobPosting() {
   }
 
   return (
-    <div class="all-container">
-    <div className="pdf-container">
-    <h1> <span style={{color: "white", fontSize: "3rem"}}> JOB </span><span style={{color: "var(--secondary)", fontSize: "3rem"}} class="fat-underline"> POSTINGS </span></h1>
-    <center>
-        <div style={{marginTop: "15px"}}>
-          <Document file="../jobposting.pdf" onLoadSuccess={onDocumentLoadSuccess}>
-            {Array.from(
-              new Array(numPages),
-              (el,index) => (
-                <Page 
-                  key={`page_${index+1}`}
-                  pageNumber={index+1}
-                />
-              )
-            )}
-          </Document>
-        </div>
-      </center>
-    </div></div>
+    <div class="no-posting-class">
+        <h1 style={{color: "black", fontSize: "4rem"}}>Currently No Jobs available</h1>
+    </div>
+    // <div class="all-container">
+    // <h1> <span style={{color: "white", fontSize: "3rem"}}> JOB </span><span style={{color: "var(--secondary)", fontSize: "3rem"}} class="fat-underline"> POSTINGS </span></h1>
+    // <div className="pdf-container">
+    // <center>
+    //   <h1 style={{color: "white"}}>Currently No Jobs available</h1>
+    //     <div style={{marginTop: "15px"}}>
+    //       <Document file="../jobposting.pdf" onLoadSuccess={onDocumentLoadSuccess}>
+    //         {Array.from(
+    //           new Array(numPages),
+    //           (el,index) => (
+    //             <Page 
+    //               key={`page_${index+1}`}
+    //               pageNumber={index+1}
+    //             />
+    //           )
+    //         )}
+    //       </Document>
+    //     </div>
+    //   </center>
+    // </div>
+    // </div>
   );
 };
